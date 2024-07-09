48e489eecf501ecbc05d2ed2b619518ef5d01e26
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2024-07-09 15:38:31 +0300

    fix app launch

--- app.py
+++ app.py
@@ -2,21 +2,15 @@ from flask import Flask
 
 from config import STATIC_DIR, TEMPLATES_DIR
 
+from endpoints.forecast import forecast_bp
+from endpoints.main import main_bp
+from endpoints.products import products_bp
 
-def create_app():
-    app = Flask(__name__, template_folder=TEMPLATES_DIR, static_folder=STATIC_DIR)
+app = Flask(__name__, template_folder=TEMPLATES_DIR, static_folder=STATIC_DIR)
 
-    from endpoints.forecast import forecast_bp
-    from endpoints.main import main_bp
-    from endpoints.products import products_bp
-
-    app.register_blueprint(forecast_bp)
-    app.register_blueprint(products_bp)
-    app.register_blueprint(main_bp)
-
-    return app
-
+app.register_blueprint(forecast_bp)
+app.register_blueprint(products_bp)
+app.register_blueprint(main_bp)
 
 if __name__ == "__main__":
-    app = create_app()
     app.run()
