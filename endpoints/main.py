722b0aa406b604850038208140f5cc17e7ab13a1
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2024-07-10 15:16:52 +0300

    endpoints update

--- endpoints/main.py
+++ endpoints/main.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 from werkzeug.exceptions import HTTPException
+
+import config
+from services.auto_process_data import auto_process_data
 
 main_bp = Blueprint("main", __name__)
 
@@ -9,12 +12,29 @@ def plot():
     return render_template("plot.html")
 
 
+@main_bp.route("/")
+def index():
+    return render_template("index.html")
+
+
+@main_bp.route("/upload")
+def upload():
+    # Endpoint to upload CSV file, then redirect to /models
+    raise NotImplementedError()
+
+
 @main_bp.route("/models", methods=["GET", "POST"])
 def create_models():
     if request.method == "POST":
         raise NotImplementedError("POST method not implemented")
     elif request.method == "GET":
-        return jsonify({"message": "Not implemented yet 123"})
+        url = request.args.get("url", config.ROSSTAT_CPI_DATA_URL)
+        try:
+            auto_process_data(data_download_url=url)
+            return redirect(url_for("main.plot"))
+        except Exception as e:
+            return jsonify({"success": False, "error": str(e)})
+
     else:
         # Handle other request methods
         raise HTTPException("Unsupported request method")
