d06fd18a9890af6d7d2a2f7ce79914748fa760da
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2024-07-10 12:09:46 +0300

    rename template

--- endpoints/main.py
+++ endpoints/main.py
@@ -1,8 +1,20 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, request, jsonify
+from werkzeug.exceptions import HTTPException
 
 main_bp = Blueprint("main", __name__)
 
 
 @main_bp.route("/plot")
-def index():
-    return render_template("index.html")
+def plot():
+    return render_template("plot.html")
+
+
+@main_bp.route("/models", methods=["GET", "POST"])
+def create_models():
+    if request.method == "POST":
+        raise NotImplementedError("POST method not implemented")
+    elif request.method == "GET":
+        return jsonify({"message": "Not implemented yet 123"})
+    else:
+        # Handle other request methods
+        raise HTTPException("Unsupported request method")
