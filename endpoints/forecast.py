00aab9b77acec0cea7b674af5a3528a7794d3384
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2024-07-10 17:15:45 +0300

    get metadata endpoint

--- endpoints/forecast.py
+++ endpoints/forecast.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 import pandas as pd
-from flask import Blueprint, send_file
+from flask import Blueprint, send_file, jsonify
 
 from services.forecast import (
     calculate_rmse,
@@ -10,12 +10,16 @@ from services.forecast import (
     get_model_dict,
     load_data,
 )
+from services.tools import slugify
 
 forecast_bp = Blueprint("forecast", __name__)
 
 
 @forecast_bp.route("/forecast/<product_name>")
 def get_forecast_plot(product_name):
+    """
+    Return plot as an image.
+    """
     data = load_data(product_name)
     model_dict = get_model_dict(product_name)
 
@@ -41,3 +45,16 @@ def get_forecast_plot(product_name):
     )
 
     return send_file(buf, mimetype="image/png")
+
+
+@forecast_bp.route("/get_metadata/<product_name>")
+def get_metadata(product_name):
+    model_dict = get_model_dict(product_name)
+    metadata = {
+        "url": f"/forecast/{slugify(product_name)}",
+        "product_name": model_dict["product_name"],
+        "train_mae": model_dict["train_mae"],
+        "test_mae": model_dict["test_mae"],
+        "date_created": model_dict["date_created"].isoformat(),
+    }
+    return jsonify(metadata)
