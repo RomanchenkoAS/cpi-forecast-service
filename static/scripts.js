bb89bc44c2297a09610e35f5c0baa143b08663c5
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2024-07-11 13:30:42 +0300

    use vanilla js instead of JQuery

--- static/scripts.js
+++ static/scripts.js
@@ -1,44 +1,55 @@
-$(document).ready(function () {
-    if ($('#product-select').length > 0) {
+document.addEventListener('DOMContentLoaded', function () {
+    const productSelect = document.getElementById('product-select');
+    if (productSelect) {
         // Load product list only if #product-select is present
-        $.getJSON('/get_products', function (products) {
-            products.forEach(function (product, index) {
-                var option = $('<option>', {
-                    value: product[1], // slugified product name
-                    text: product[0]  // original product name
-                });
-                $('#product-select').append(option);
+        fetch('/get_products')
+            .then(response => response.json())
+            .then(products => {
+                products.forEach((product, index) => {
+                    const option = document.createElement('option');
+                    option.value = product[1]; // slugified product name
+                    option.text = product[0];  // original product name
+                    productSelect.appendChild(option);
 
-                // Set the first option as selected
-                if (index === 0) {
-                    option.attr('selected', 'selected');
-                }
+                    // Set the first option as selected
+                    if (index === 0) {
+                        option.selected = true;
+                    }
+                });
             });
-        });
 
         // Tie getForecast function to the button
-        $('#get-forecast').click(getForecast);
+        document.getElementById('get-forecast').addEventListener('click', getForecast);
     }
 });
 
-
 function getForecast() {
-    var productName = $('#product-select').val();
+    const productName = document.getElementById('product-select').value;
     if (productName) {
         // Get metadata
-        $.getJSON('/get_metadata/' + encodeURIComponent(productName), function (metadata) {
-            var metadataHtml = `
-                <h6>Metadata:</h6>
-                <p>Product: ${metadata.product_name}</p>
-                <p>Train MAE: ${metadata.train_mae.toFixed(2)}</p>
-                <p>Test MAE: ${metadata.test_mae.toFixed(2)}</p>
-                <p>Date Created: ${new Date(metadata.date_created).toLocaleString()}</p>
-            `;
-            $('#metadata').html(metadataHtml);
-        });
+        fetch('/get_metadata/' + encodeURIComponent(productName))
+            .then(response => response.json())
+            .then(metadata => {
+                console.log(metadata);
+                document.getElementById('metadata').innerHTML = `
+                    <h6>Metadata:</h6>
+                    <p>Product: ${metadata.product_name}</p>
+                    <p>Date Created: ${metadata.date_created}</p>
+                    <p>Train MAE: ${metadata.train_mae.toFixed(2)}</p>
+                    <p>Test MAE: ${metadata.test_mae.toFixed(2)}</p>
+                    <hr>
+                    <p>Confidence interval estimation</p>
+                    <p>Avg in-sample CI: ${metadata.avg_in_sample_ci_width.toFixed(2)}</p>
+                    <p>Max in-sample CI: ${metadata.max_in_sample_ci_width.toFixed(2)}</p>
+                    <p>Avg forecast CI: ${metadata.avg_forecast_ci_width.toFixed(2)}</p>
+                    <p>Max forecast CI: ${metadata.max_forecast_ci_width.toFixed(2)}</p>
+                `;
+            });
 
         // Load forecast image
-        $('#forecast-image').attr('src', '/forecast/' + encodeURIComponent(productName)).show();
+        const forecastImage = document.getElementById('forecast-image');
+        forecastImage.src = '/forecast/' + encodeURIComponent(productName);
+        forecastImage.style.display = 'block';
     }
 }
 
